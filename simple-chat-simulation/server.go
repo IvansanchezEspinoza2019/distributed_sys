1f437aa5e1f8be5c9e1e999e1405c08c30caafcc
Ivan Sanchez <edgarse1945@gmail.com>
2021-10-02 20:52:02 -0500

    Code update

--- simple-chat-simulation/server.go
+++ simple-chat-simulation/server.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/gob"
 	"errors"
 	"fmt"
 	"net"
 )
 
 type Server struct {
-	Host string
+	Host    string
+	Clients []net.Conn
 }
 
 func (s *Server) Init() {
@@ -36,16 +38,52 @@ func (s *Server) Run() error {
 }
 
 func (s *Server) HandleClient(c net.Conn) {
-	fmt.Println(c)
+	defer c.Close()
+
+	/* Add the new client to the slice */
+	s.Clients = append(s.Clients, c)
+
+	fmt.Println(s.Clients)
+
+	var instruction string
+	var msg string
+
+	/* Listen for client requests */
+	for {
+		/*1.- receive the instruction (post_msg, etc) */
+		receive := gob.NewDecoder(c)
+		err := receive.Decode(&instruction)
+		if err == nil {
+			fmt.Println("Instruccion recibida: ", instruction)
+
+			if instruction == "post_msg" {
+				/* 2.- reads the data   */
+				receibeMsg := gob.NewDecoder(c)
+				errMsg := receibeMsg.Decode(&msg)
+
+				/* error*/
+				if errMsg == nil {
+					fmt.Println("Mensaje recibido: ", msg)
+				} else {
+					fmt.Println("Error ", err)
+					return
+				}
+			}
+
+		} else { /* error */
+			fmt.Println("Error ", err)
+			return
+		}
+	}
+
 }
 
 func main() {
-
+	/* serever init */
 	s := Server{Host: ":5555"}
-	//s.Init()
+
 	go s.Run()
 
 	var in string
 	fmt.Scanln(&in)
-
 }

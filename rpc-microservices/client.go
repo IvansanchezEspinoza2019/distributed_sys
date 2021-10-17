f95604f59cc83e30aeaa4986e21ee3b40b3d55ad
Ivan Sanchez <edgarse1945@gmail.com>
2021-10-17 11:48:19 -0500

    Add students notes implemented

--- rpc-microservices/client.go
+++ rpc-microservices/client.go
@@ -7,6 +7,19 @@ import (
 	"os"
 )
 
+/* server response */
+type Reply struct {
+	Msg    string
+	Status int
+}
+
+/* struct to save client request for saving a student note*/
+type StudentSubject struct {
+	Name    string
+	Subject string
+	Note    float64
+}
+
 func client() {
 	c, err := rpc.Dial("tcp", "127.0.1.1:9999")
 
@@ -24,11 +37,20 @@ func client() {
 		opc = scanner.Text()
 
 		if opc == "1" {
-			var name, response string
-			fmt.Print("Name: ")
-			fmt.Scanln(&name)
+			var response Reply
+			data := &StudentSubject{}
 
-			err = c.Call("MicroService.Hello", name, &response)
+			fmt.Print("Name: ")
+			fmt.Scanln(&data.Name)
+
+			fmt.Print("Subject: ")
+			fmt.Scanln(&data.Subject)
+
+			fmt.Print("Note: ")
+			fmt.Scanf("%f", &data.Note)
+			fmt.Scanln()
+
+			err = c.Call("MicroService.AddNoteStudentSubjetc", data, &response)
 			if err != nil {
 				fmt.Println(err)
 			} else {
@@ -44,6 +66,6 @@ func main() {
 
 func menu() {
 	fmt.Println("\n.: Menu :.\n")
-	fmt.Println("1 ) Hello")
+	fmt.Println("1 ) Agregar nota de estudiante")
 	fmt.Print("Opcion: ")
 }
--- rpc-microservices/server.go
+++ rpc-microservices/server.go
@@ -7,18 +7,64 @@ import (
 	"net/rpc"
 )
 
-type MicroService struct {
+/* struct to save client request for saving a student note*/
+type StudentSubject struct {
+	Name    string
+	Subject string
+	Note    float64
 }
 
-func (m *MicroService) Hello(name string, response *string) error {
-	*response = "Hello " + name + "!!"
+/* server response */
+type Reply struct {
+	Msg    string
+	Status int
+}
+
+/* struct microservices */
+type MicroService struct {
+	Students map[string]map[string]float64
+	Subjects map[string]map[string]float64
+	// ejemplo: https://stackoverflow.com/questions/68281518/golang-rpc-get-wrong-struct-variable
+}
+
+/* CALLS*/
+func (m *MicroService) AddNoteStudentSubjetc(data *StudentSubject, response *Reply) error {
+
+	// add student info
+	if _, exists := m.Students[data.Name]; exists { //  already exists
+		if exists {
+			m.Students[data.Name][data.Subject] = data.Note
+		}
+	} else {
+		subject := make(map[string]float64)
+		subject[data.Subject] = data.Note
+		m.Students[data.Name] = subject
+	}
+
+	// add subject info
+	if _, exists := m.Subjects[data.Subject]; exists {
+		if exists {
+			m.Subjects[data.Subject][data.Name] = data.Note
+		}
+	} else {
+		// create the first student
+		student := make(map[string]float64)
+		student[data.Name] = data.Note
+		// create the subject
+		m.Subjects[data.Subject] = student
+	}
+	fmt.Println("STUDENTS: ", m.Students)
+	fmt.Println("SUBJECTS: ", m.Subjects)
+	response.Msg = "EXITO"
+	response.Status = 200
 	return nil
 }
 
+/* Server Struct*/
 type Server struct {
 	Host    string
-	Service MicroService
 	Listen  net.Listener
+	Service *MicroService
 }
 
 func (s *Server) Init() error {
@@ -40,7 +86,13 @@ func (s *Server) Init() error {
 }
 
 func (s *Server) Run() {
-	rpc.Register(new(MicroService))
+	/* instance of microservices struct*/
+	st := make(map[string]map[string]float64)
+	sb := make(map[string]map[string]float64)
+	s.Service = &MicroService{Students: st, Subjects: sb}
+
+	rpc.Register(s.Service)
+	/* listen for clients */
 	for {
 		c, err := s.Listen.Accept()
 		if err != nil {
@@ -51,7 +103,13 @@ func (s *Server) Run() {
 	}
 }
 
+func (s *Server) Hello(name string, response *string) error {
+	*response = "Hello Server " + name + " Host " + s.Host
+	return nil
+}
+
 func main() {
+	// init server
 	server := Server{Host: ":9999"}
 	err := server.Init()
 	if err != nil {
